39f7d79c46b6f311ecc5eb47714a60a581ce68bb
Tarek Ziade <tarek@ziade.org>
2014-07-26 23:33:44 +0200

    display friends client_id

--- toxmail/dashboard/__init__.py
+++ toxmail/dashboard/__init__.py
@@ -9,8 +9,17 @@ loader = template.Loader(os.path.dirname(__file__))
 
 class DashboardHandler(tornado.web.RequestHandler):
     def get(self):
-        tox_id = self.application.tox.get_address()
-        resp = loader.load("index.html").generate(tox_id=tox_id)
+        tox = self.application.tox
+        client_id = tox.get_address()
+        friends = []
+
+        for friend_id in tox.get_friendlist():
+            friend = {'friend_id': friend_id}
+            friend['client_id'] = tox.get_client_id(friend_id)
+            friends.append(friend)
+
+        resp = loader.load("index.html").generate(client_id=client_id,
+                                                  friends=friends)
         self.write(resp)
 
 
