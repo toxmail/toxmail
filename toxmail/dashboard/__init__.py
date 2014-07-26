42d064f3ddc27b848e99084fb7e20c73e864d35c
Tarek Ziade <tarek@ziade.org>
2014-07-27 00:42:09 +0200

    let users remove friends

--- toxmail/dashboard/__init__.py
+++ toxmail/dashboard/__init__.py
@@ -2,6 +2,7 @@ import os
 
 import tornado.web
 from tornado import template
+from tox import OperationFailedError
 
 
 loader = template.Loader(os.path.dirname(__file__))
@@ -26,14 +27,29 @@ class DashboardHandler(tornado.web.RequestHandler):
 
 
 class FriendHandler(tornado.web.RequestHandler):
-    def get(self):
-        client_id = self.request.query_arguments['client_id'][0]
-        self.application.tox.add_friend_norequest(client_id)
-        self.application.tox.save()
+    def post(self):
+        client_id = self.request.body_arguments['client_id'][0].strip()
+        # XXX: make sure client id is a 64 long key
+        tox = self.application.tox
+
+        if 'add' in self.request.body_arguments:
+            try:
+                tox.add_friend_norequest(client_id)
+            except OperationFailedError:
+                # XXX we want an error message
+                pass
+            else:
+                tox.save()
+        elif 'delete' in self.request.body_arguments:
+            friend_id = tox.get_friend_id(client_id)
+            tox.del_friend(friend_id)
+            tox.save()
+
         self.redirect('/')
 
 
 application = tornado.web.Application([
     (r"/", DashboardHandler),
-    (r"/add_friend", FriendHandler)
+    (r"/friend", FriendHandler),
+    (r"/friend/(.*)", FriendHandler)
 ])
