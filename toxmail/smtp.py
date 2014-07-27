819a36c87c478d8d5ca940898d5744c623a64c4c
Tarek Ziade <tarek@ziade.org>
2014-07-27 13:50:14 +0200

    fixed the smtp service

--- toxmail/smtp.py
+++ toxmail/smtp.py
@@ -1,10 +1,17 @@
 from bonzo.server import SMTPServer as _SMTPServer
+from bonzo.server import SMTPConnection as _SMTPConnection
 from pyzmail import PyzMessage
 
 
 class SMTPServer(_SMTPServer):
+    def handle_stream(self, stream, address):
+        SMTPConnection(stream, address, self.request_callback)
+
+
+class SMTPConnection(_SMTPConnection):
     def _on_data(self, data):
-        msg = PyzMessage.factory(payload)
+        data = str(data)
+        msg = PyzMessage.factory(data)
         try:
             self.request_callback(msg)
         except Exception, e:
